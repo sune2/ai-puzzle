9ccf6a0e69dafccf3ef6e0e20acc37744a2fbe7b
Tamura Kazunori <sunes232002@gmail.com>
2019-07-10 10:31:01 +0900

    リファクタ

--- Assets/Puzzle/Puzzle.cs
+++ Assets/Puzzle/Puzzle.cs
@@ -1,21 +1,19 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
-using Random = UnityEngine.Random;
 
 public class Puzzle : MonoBehaviour
 {
-    public const int Width = 4;
-    public const int Height = 4;
+    private const int Width = 4;
+    private const int Height = 4;
     public const int Area = Width * Height;
 
     public static readonly int Empty = Width * Height - 1;
-
-    [SerializeField]
-    private bool _showView;
+    
+    private static readonly int[] dy = {-1, 0, 1, 0};
+    private static readonly int[] dx = {0, 1, 0, -1};
 
     [SerializeField]
     private PuzzleCellView _cellViewPrefab;
@@ -25,10 +23,8 @@ public class Puzzle : MonoBehaviour
 
     private PuzzleCellView[][] _cellViews;
     private int[][] _puzzle;
-    
-    private ValueTuple<int, int>[] _correctPositions;
 
-    private Vector2Int _zeroPos;
+    private Vector2Int _emptyPos;
 
     // Use this for initialization
     void Awake()
@@ -36,21 +32,15 @@ public class Puzzle : MonoBehaviour
         // Time.timeScale = 0.05f;
         _gridLayoutGroup.constraintCount = Width;
         _cellViews = new PuzzleCellView[Height][];
-        _correctPositions = new ValueTuple<int, int>[Area];
         for (int i = 0; i < Height; i++)
         {
             _cellViews[i] = new PuzzleCellView[Width];
             for (int j = 0; j < Width; j++)
             {
                 var cellView = Instantiate(_cellViewPrefab, _gridLayoutGroup.transform, false);
-                cellView.Initialize(i, j, _showView);
-                cellView.onClicked += () => OnClickedCell(cellView);
                 _cellViews[i][j] = cellView;
-                _correctPositions[i * Width + j] = ValueTuple.Create(i, j); 
             }
         }
-
-        // Initialize();
     }
 
 //    private void Update()
@@ -75,7 +65,7 @@ public class Puzzle : MonoBehaviour
 
     public void Initialize()
     {
-        var cnt = 0;
+        var cnt = 1;
         for (int i = 0; i < Height; i++)
         {
             for (int j = 0; j < Width; j++)
@@ -85,8 +75,11 @@ public class Puzzle : MonoBehaviour
             }
         }
 
-        _zeroPos = new Vector2Int(Width - 1, Height - 1);
+        // 右下のセルは0にして空白を表現する
+        _cellViews[Height - 1][Width - 1].SetNumber(0);
+        _emptyPos = new Vector2Int(Width - 1, Height - 1);
 
+        // 適当な回数ランダムでスライドして初期配置を作る
         for (int i = 0; i < Area * Area * 3; i++)
         {
             Slide(Random.Range(0, 4));
@@ -109,74 +102,23 @@ public class Puzzle : MonoBehaviour
 
     public bool Slide(int dir)
     {
-        var x = _zeroPos.x + dx[dir];
-        var y = _zeroPos.y + dy[dir];
+        var x = _emptyPos.x + dx[dir];
+        var y = _emptyPos.y + dy[dir];
 
         if (x < 0 || x >= Width || y < 0 || y >= Height) return false;
 
-        var cellView = _cellViews[_zeroPos.y][_zeroPos.x];
+        var cellView = _cellViews[_emptyPos.y][_emptyPos.x];
         int number = _cellViews[y][x].Number;
         _cellViews[y][x].SetNumber(cellView.Number);
         cellView.SetNumber(number);
 
-        _zeroPos = new Vector2Int(x, y);
+        _emptyPos = new Vector2Int(x, y);
 
         return true;
     }
 
-    private static readonly int[] dy = {-1, 0, 1, 0};
-    private static readonly int[] dx = {0, 1, 0, -1};
-
-    private void OnClickedCell(PuzzleCellView cellView)
-    {
-        for (int k = 0; k < 4; k++)
-        {
-            int y = cellView.Y + dy[k];
-            int x = cellView.X + dx[k];
-            if (y >= 0 && y < Height && x >= 0 && x < Width)
-            {
-                if (_cellViews[y][x].Number == Empty)
-                {
-                    int number = _cellViews[y][x].Number;
-                    _cellViews[y][x].SetNumber(cellView.Number);
-                    cellView.SetNumber(number);
-                    _zeroPos = new Vector2Int(cellView.X, cellView.Y);
-                }
-            }
-        }
-    }
-
-    public int SameCount()
-    {
-        var numbers = GetCellNumbers();
-        var cnt = 0;
-        for (int i = 0; i < Width * Height; i++)
-        {
-            if (numbers[i] == i) cnt++;
-        }
-
-        return cnt;
-    }
-
-    public int InversionNumber()
-    {
-        var numbers = GetCellNumbers();
-        var cnt = 0;
-        for (int i = 1; i < Width * Height; i++)
-        {
-            for (int j = 0; j < i; j++)
-            {
-                if (numbers[i] < numbers[j]) cnt++;
-            }
-        }
-
-        return cnt;
-    }
-
     public int GetFirstDifferentIndex()
     {
-        // var numbers = GetCellNumbers();
-
         var cnt = 0;
         for (int i = 0; i < Height; i++)
         {
@@ -186,27 +128,11 @@ public class Puzzle : MonoBehaviour
                 {
                     return cnt;
                 }
+
                 cnt++;
             }
         }
 
         return Area;
-    }
-    
-    public int GetDistanceSum()
-    {
-        int res = 0;
-        for (int i = 0; i < Height; i++)
-        {
-            for (int j = 0; j < Width; j++)
-            {
-                var number = _cellViews[i][j].Number;
-                int y = _correctPositions[number].Item1;
-                int x = _correctPositions[number].Item2;
-                res += Mathf.Abs(i - y) + Mathf.Abs(j - x); 
-            }
-        }
-
-        return res;
     }
 }
--- Assets/Puzzle/PuzzleCellView.cs
+++ Assets/Puzzle/PuzzleCellView.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Serialization;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class PuzzleCellView : MonoBehaviour
@@ -10,31 +6,11 @@ public class PuzzleCellView : MonoBehaviour
     [SerializeField]
     private Text _label;
 
-    [SerializeField]
-    private Button _button;
-
-    public Action onClicked;
-
-    private bool _showView;
-    
-    public int Y { get; private set; }
-    public int X { get; private set; }
     public int Number { get; private set; }
-
-    public void Initialize(int y, int x, bool showView)
-    {
-        Y = y;
-        X = x;
-        _showView = showView;
-        _button.onClick.AddListener(() => onClicked?.Invoke());
-    }
 
     public void SetNumber(int number)
     {
-                if (_showView)
-        {
-            _label.text = $"{(number < Puzzle.Empty ? (number + 1).ToString() : "")}";
-        }
+        _label.text = $"{(number == 0 ? (number + 1).ToString() : "")}";
         Number = number;
     }
 }
