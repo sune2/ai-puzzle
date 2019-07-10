86aff3044e170eb4d4630c46189a1a56573dc327
Kazunori Tamura <tamura_kazunori@cyberagent.co.jp>
2019-07-10 21:47:17 +0900

    更に整理

--- Assets/Puzzle/PuzzleAcademy.cs
+++ Assets/Puzzle/PuzzleAcademy.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using MLAgents;
-using UnityEngine;
-using UnityEngine.UI;
+﻿using MLAgents;
 
 public class PuzzleAcademy : Academy
 {
-    
 }
